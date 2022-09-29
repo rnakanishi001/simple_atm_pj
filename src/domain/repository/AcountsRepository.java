f97bc6d07c43ab3502aa7ced3542762d021c37ae
rnakanishi001 <rnakanishi@ities.co.jp>
2022-09-29 18:45:29 +0900

    口座リポジトリ修正

--- src/domain/repository/AcountsRepository.java
+++ src/domain/repository/AcountsRepository.java
@@ -1,10 +1,5 @@
 package domain.repository;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import domain.entity.Acount;
-
 public class AcountsRepository {
-    private List<Acount> acounts = new LinkedList<>();
+    
 }
